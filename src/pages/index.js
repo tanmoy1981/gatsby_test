dd6db05200d9ec88fb6b040b2e486fb758a0308a
cpp git <cppgit@outlook.com>
2022-08-31 15:55:01 +0530

    Listed articles in gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,20 @@ import React from "react"
 import { Link, graphql } from "gatsby"
 
 const IndexPage = ({ data }) => (
-  <p>
+  <article>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
 
-    <Link to="/page-2/">Go to page 2</Link>
-  </p>
+    <ul>
+      {data.allStrapiArticle.edges.map(document => (
+        <li key={document.node.id}>
+          <h2>{document.node.title}</h2>
+          <p>{document.node.content}</p>
+        </li>
+      ))}
+    </ul>
+  </article>
 )
 
 export default IndexPage
